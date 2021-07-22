5f715a716be6b50e8b91e2c39bf38b6d885ab640
unknown <adityashakya836@gmail.com>
2021-07-23 00:51:30 +0530

    make it better

--- edu_website/settings.py
+++ edu_website/settings.py
@@ -166,6 +166,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, '/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 SITE_ID = 3
